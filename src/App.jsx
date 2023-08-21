f1072241059be78113dea070502a1ac5a62957a1
Manuel Murillo <mmpeira@gmail.com>
2023-08-21 14:03:28 +0200

    nav, hamburger, hero sections done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
+import {
+  CustomerReviews,
+  PopularProducts,
+  Hero,
+  Footer,
+  SuperQuality,
+  Services,
+  SpecialOffers,
+  Subscribe,
+} from "./sections";
+import Nav from "./components/Nav";
+
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav/>
     <section className="xl:padding-1 wide:padding-r padding-b">
-      
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffers />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8 text-white">
+      <Footer />
     </section>
   </main>
 );
