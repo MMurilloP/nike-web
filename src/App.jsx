864381ae258dcba3f40c6730a582abcfeb703fc7
Manuel Murillo <mmpeira@gmail.com>
2023-08-21 13:00:25 +0200

    build proyect done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      
+    </section>
+  </main>
+);
 
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+export default App;
